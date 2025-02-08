10f38f029ff32fd90cf31f808336ea27e492bf4a
Lev Khrobostov <lev.khrobostov@list.ru>
2025-02-08 15:14:47 +0300

    asdasdas

--- Game.java
+++ Game.java
@@ -1,9 +1,15 @@
 package test;
 
 public class Game {
+
+    public static int health = 0;
+    public static String da = "da";
+
+
     public static void main(String[] args) {
         
     }
+    
     public void Sosal(){
         
     }
